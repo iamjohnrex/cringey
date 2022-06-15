ccf7459d61ec45fe80c442e81777e06bf920545d
iamjohnrex <46390647+iamjohnrex@users.noreply.github.com>
2022-06-15 22:59:52 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>test hyperlink</title>
+    <style type="text/css">
+        a{
+            font-family: "Helvetica";
+            font-weight: bolder;
+            font-size: 16px;
+            background: #FFFFE0;
+        }
+        body {
+            text-align: center;
+            line-height: 100vh;
+        }
+    </style>
+</head>
+<body>
+<a href="https://form.jotform.com/221248111787051" target="_blank" rel="noreferrer">CLICK HERE</a>
+</body>
+</html>
